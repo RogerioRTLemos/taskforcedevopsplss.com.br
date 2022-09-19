203063af20c3a24e691629696e3a559568d9bb53
Rogerio Lemos <rogeriortlemos@gmail.com>
2022-09-19 11:18:57 -0300

    adicionado html para testar

--- testando123.php
+++ testando123.php
@@ -1,1 +1,19 @@
-teste
+<html>
+    <head>
+        <title>Task Force Devops PLSS</title>
+        <link href="css/style.css" rel="stylesheet" />
+        <meta name="vieweport" content="width=devide-widht, initial-scale=1.0, maximum-scale=1.0">
+    </head>
+    <body>
+        <header>
+            <div class="center">
+                <div class="logo"><img src="assets/logo2.png" /></div><!--logo-->
+                <div class="menu">
+                    <a href="index.php">Home</a>
+                    <a href="aleatorio.php">Aleatorio</a>
+                    <a href="completo.php">Tempo</a>
+                    <a href="cep.php">CEP</a>
+                    <a href="#">teste</a>
+                </div><!--menu-->
+            </div><!--center---->
+        </header>
